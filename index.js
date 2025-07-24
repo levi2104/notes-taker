d17b6a4e9e2ac90a9cb019e91cd5437406dcea8f
Aryan Tanna <116259512+levi2104@users.noreply.github.com>
2025-07-24 13:11:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -26,4 +26,14 @@ app.get("/file/:filename", (req, res) => {
   });
 });
 
+app.get('/edit/:filename', (req, res) => {
+  res.render('edit', {filename: req.params.filename})
+})
+
+app.post('/edit', (req, res) => {
+  fs.rename(`./files/${req.body.previous}`, `./files/${req.body.new}`, (err) => {
+    res.redirect('/')
+  })
+})
+
 app.listen(3000)
